bd2a03a204784ee88c3f0b99dd01bcdf5acb6706
jacob <jacobmalmberg@gmail.com>
2019-12-18 16:54:28 +0100

    fixed propagate

--- propagate.m
+++ propagate.m
@@ -2,23 +2,18 @@ function particles = propagate(particles,sigma, imgSize)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 M = size(particles,1);
-var_x = 1000;
-var_y = 1500;
 
-
-noiseX = mvnrnd(zeros(size(sigma)), var_x, M);
-noiseY = mvnrnd(zeros(size(sigma)), var_y, M);
+noiseY = mvnrnd(0, sigma(1), M);
+noiseX = mvnrnd(0, sigma(2), M);
 %particles(:,1:2) = round(particles(:,1:2) + noise);
 
-velx = 2*randn(M, 1).*sqrt(2500);
-vely = 1*randn(M, 1).*sqrt(2500);
 
 w = imgSize(1);
 h = imgSize(2);
 
 
-particles(:,1) = round(particles(:,1) + noiseX );%+ velx);
-particles(:,2) = round(particles(:,2) + noiseY );%+ vely);
+particles(:,1) = round(particles(:,1) + noiseY );%+ velx);
+particles(:,2) = round(particles(:,2) + noiseX );%+ vely);
 
 particles(:,1) = min(max(particles(:,1), 1), w);
 particles(:,2) = min(max(particles(:,2), 1), h);
